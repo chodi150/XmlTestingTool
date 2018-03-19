4121fdc827744ee59b71c25a89278922df121cc5
Apokrbk <damian.portasinski@gmail.com>
2018-03-19 20:10:04 +0100

    init

---
+++ src/main/java/com/example/projektpik/ProjektpikApplication.java
@@ -0,0 +1,12 @@
+package com.example.projektpik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektpikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektpikApplication.class, args);
+	}
+}
