d84dcf3251643c31faca6c49061ca5802ed9b6a0
marcin <mszulc913@gmail.com>
2018-04-08 18:58:19 +0200

    Initial test forms

---
+++ src/main/java/com/example/projektpik/xml/XMLConfig.java
@@ -0,0 +1,33 @@
+//package com.example.projektpik.xml;
+//
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.thymeleaf.spring5.SpringTemplateEngine;
+//import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+//
+//
+//@Configuration
+//public class XMLConfig
+//{
+//    @Bean
+//    SpringResourceTemplateResolver xmlTemplateResolver(ApplicationContext appCtx) {
+//        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+//
+//        templateResolver.setApplicationContext(appCtx);
+//        templateResolver.setPrefix("classpath:/templates/");
+//        templateResolver.setSuffix(".xml");
+//        templateResolver.setTemplateMode("XML");
+//        templateResolver.setCharacterEncoding("UTF-8");
+//        templateResolver.setCacheable(false);
+//
+//        return templateResolver;
+//    }
+//
+//    @Bean(name="springTemplateEngine")
+//    SpringTemplateEngine templateEngine(ApplicationContext appCtx) {
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//        templateEngine.setTemplateResolver(xmlTemplateResolver(appCtx));
+//        return templateEngine;
+//    }
+//}
