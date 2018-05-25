6bd4b12b98e990e5447d068c52fb0c160b417d3d
Piotr Chodyko <piotr.chodyko@gmail.com>
2018-05-25 20:09:55 +0200

    Poprawka pomów

--- WebApp/src/main/java/com/example/projektpik/ProjektpikApplication.java
+++ WebApp/src/main/java/com/example/projektpik/ProjektpikApplication.java
@@ -1,28 +1,14 @@
 package com.example.projektpik;
 
 import business.TagInjector;
-import business.XMLValidator;
-import com.example.projektpik.database.XmlDocumentRepository;
 import com.example.projektpik.models.Student;
 import config.XMLConfig;
-import exception.NotValidXmlException;
-import file_handling.XMLFile;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.validation.Validator;
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
