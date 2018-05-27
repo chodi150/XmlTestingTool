60fa2cfbb95c9bd5da8bda879e39bbda14ddb803
Piotr Chodyko <pchodyko@bluesoft.net.pl>
2018-05-27 13:39:00 +0200

    Zrobienie fasady, walidacja xmla przy update

--- XMLTestingTool/src/main/java/exception/NotValidXmlException.java
+++ XMLTestingTool/src/main/java/exception/NotValidXmlException.java
@@ -4,6 +4,10 @@ package exception;
  * Created by Piotr on 19.05.2018.
  */
 public class NotValidXmlException extends Exception {
+    public NotValidXmlException(String message) {
+        super(message);
+    }
+
     public NotValidXmlException(String message, Throwable cause) {
         super(message, cause);
     }
